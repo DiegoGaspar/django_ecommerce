4d5114e3d142ecf7912da6b55fc6adadf5559d24
Diego Gaspar <diego25gaspar@gmail.com>
2019-09-20 08:32:19 -0300

    create pages about and contact

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -2,4 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title": "Página Principal",
+        "content":"Bem vindo a página principal"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title": "Página Sobre",
+        "content":"Bem vindo a página sobre"
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página de contato",
+        "content":"Bem vindo a página contato"
+    }
+    return render(request, "contact/view.html", context)
